4bd63ae20804cf863571b35f6595ae483b1911f2
Mark Winick <mark_winick@yahoo.com>
2019-06-22 14:56:08 -0500

    2-56

--- image-finder/src/main/java/Main.java
+++ image-finder/src/main/java/Main.java
@@ -1,15 +1,17 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class Main {
+public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "NTkyMDcxNzQ1MzY4ODgzMjAx.XQ6BEg.4R-ELWcxc6fJfXHMvykbDi7Yjiw";
         builder.setToken(token);
+        builder.addEventListener(new Main());
         builder.buildAsync();
 
     }
@@ -19,6 +21,9 @@ public class Main {
         System.out.println("We received a message from " +
                 event.getAuthor().getName()+ ": " +
                 event.getMessage().getContentDisplay());
+        if (event.getMessage().getContentRaw().equals("test")) {
+            event.getChannel().sendMessage("test").queue();
+        }
     }
 
 }
