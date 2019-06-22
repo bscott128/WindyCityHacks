9cd596bbd81d0a8e088e52db5d912c67ce853c84
Mark Winick <mark_winick@yahoo.com>
2019-06-22 14:13:46 -0500

    2-23pm
    imported wrapper

---
+++ image-finder/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+    }
+
+}
