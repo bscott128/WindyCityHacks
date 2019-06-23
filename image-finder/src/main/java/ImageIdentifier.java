95126c99061edfb3ca3694093c5c7ead06fd6246
Mark Winick <mark_winick@yahoo.com>
2019-06-22 19:52:25 -0500

    7-52

--- image-finder/src/main/java/ImageIdentifier.java
+++ image-finder/src/main/java/ImageIdentifier.java
@@ -1,12 +1,5 @@
-import org.opencv.core.*;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-//import org.opencv.highgui.Highgui;
-import org.opencv.objdetect.CascadeClassifier;
+
 
 public class ImageIdentifier {
-    
+
 }
