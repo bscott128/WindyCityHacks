b8a3e779de8d1bae2e0099ee221fab94e4918fcd
Mark Winick <mark_winick@yahoo.com>
2019-06-23 01:27:30 -0500

    1-27

--- discord-bot/src/main/java/TTTGame.java
+++ discord-bot/src/main/java/TTTGame.java
@@ -1,17 +1,29 @@
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class TTTGame {
     public static int state = 0; //0-asking question, 1-writing question
-    public static byte[][] table;
     public static final byte ONE = 0b1;
     public static final byte ZERO = 0b10;
     public static final byte DEFAULT = 0b0;
+    public static String[][] table;
     public TTTGame() {
-        table = new byte[3][3];
+        table = new String[3][3];
+        for (int i = 0; i<table.length;i++) {
+            for (int j = 0; j<table[i].length;j++) {
+                table[i][j] = " ";
+            }
+        }
     }
     public static final String visualTable =
-            "```  \n" +
-            "  \n" +
-            "  ```";
+            "```      |   |\n" +
+                    " 3 "+table[0][0]+" | "+table[0][1]+" | "+table[0][2]+ "\n" +
+                    "   ___|___|___\n" +
+                    "      |   |\n" +
+                    " 2  "+table[1][0]+" | "+table[1][1]+" | "+table[1][2]+"\n" +
+                    "   ___|___|___\n" +
+                    "      |   |\n" +
+                    " 1  "+table[2][0]+" | "+table[2][1]+" | "+table[2][2]+"\n" +
+                    "      |   |\n" +
+                    "    A   B   C```";
 
     public void next(MessageReceivedEvent event) {
         String message = event.getMessage().getContentRaw().toLowerCase();
