333817d6e3f6b070ac1f042c6e49ddca9f135344
willschlach <will.p.schlach@gmail.com>
2019-06-23 03:09:34 -0500

    ad

---
+++ discord-bot/src/main/java/Main.java
@@ -0,0 +1,63 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.*;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.util.*;
+import java.io.*;
+
+public class Main extends ListenerAdapter {
+    public static UserInterface u;
+    public static String currentCommand;
+    private Trivia t;
+
+    public static void main(String[] args) throws LoginException {
+        u = new UserInterface();
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "NTkyMDcxNzQ1MzY4ODgzMjAx.XQ6BEg.4R-ELWcxc6fJfXHMvykbDi7Yjiw";
+        builder.setToken(token);
+        builder.addEventListener(new Main());
+        builder.build();
+
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if(event.getMessage().getContentRaw().toLowerCase().equals("%trivia"))
+            t = new Trivia(event);
+        t.playGame(event);
+        String message = event.getMessage().getContentRaw().toLowerCase();
+        currentCommand = message;
+        if (!message.equals("")&&(!event.getAuthor().isBot()))
+            u.processCommand(event);
+        System.out.println("We received a message from " +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay());
+        List<Message.Attachment> attatchments = event.getMessage().getAttachments();
+        if (attatchments.size() > 0) {
+            List<Message.Attachment> images = new LinkedList();
+            for (Message.Attachment mess : attatchments) {
+                if (mess.isImage()) {
+                    images.add(mess);
+                }
+            }
+            for (Message.Attachment messageImage : images) {
+                String fileName = messageImage.getFileName();
+                int period = fileName.indexOf(".");
+                String fileType = fileName.substring(period);
+                int hash = messageImage.hashCode();
+                File tempFile = new File("./images/" + hash + fileType);
+                messageImage.download(tempFile);
+                event.getChannel().sendMessage("Image downloaded as " + hash + fileType).queue();
+            }
+            event.getChannel().sendMessage("" + images.size() + " images downloaded").queue();
+        }
+    }
+
+
+
+}
