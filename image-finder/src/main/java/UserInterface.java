ba29b1abac3e095de9ace6d420ddac309447e61f
bscott128 <52081759+bscott128@users.noreply.github.com>
2019-06-22 21:19:03 -0500

    9-18

--- image-finder/src/main/java/UserInterface.java
+++ image-finder/src/main/java/UserInterface.java
@@ -39,6 +39,7 @@ public class UserInterface{
 
     private void declareCommands(){
         commands.add("help");
+
         //File folder = new File(".");
         //File[] listFiles = folder.listFiles();
 
